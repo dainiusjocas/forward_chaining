ac500e9324cffe80d078dc011117e213f2656718
Dainius Jocas <dainiusjocas@gmail.com>
2011-03-16 23:05:18 +0100

    implemented data collector from command line

--- src/forwardchaining/Client.java
+++ src/forwardchaining/Client.java
@@ -1,5 +1,7 @@
 package forwardchaining;
 
+import java.util.ArrayList;
+
 /**
  * Sample application of Forward chaining
  * @author Dainius Jocas, VU MIF, PS#3, 3rd year
@@ -11,22 +13,22 @@ public class Client {
                 "chaining works started to work:\n");
         String fileURI = "src/xml/4_9_implications.xml";
         try{
-            DataCollector dc = new DataCollectorFromCommandLine();
-//            Facts facts = new Facts(dc.collectFacts());
+            DataCollector dc = new DataCollectorFromCommandLine();            
+            Facts facts = new Facts(dc.collectFacts());
             String goal = dc.collectGoal();
-//            ListOfImplications loi =
-//                    new ListOfImplications(dc.collectImplications());
-//            System.out.println("Data for the program has been taken from " +
-//                    fileURI + " file.\n");
-//            DerivationMachine dm = new DerivationMachine(loi, facts, goal);
-//            dm.showData();
-//            if (dm.doForwardChaining()) {
-//                System.out.println("GOAL IS REACHED!\n" + "Final list of facts is: "
-//                    + dm.getListOfFacts());
-//                System.out.println("Production System: " + dm.getCurrentProductionSystem());
-//            } else {
-//                System.out.println("The goal is not reached.");
-//            }
+            ListOfImplications loi =
+                    new ListOfImplications(dc.collectImplications());
+            System.out.println("Data for the program has been taken from " +
+                    fileURI + " file.\n");
+            DerivationMachine dm = new DerivationMachine(loi, facts, goal);
+            dm.showData();
+            if (dm.doForwardChaining()) {
+                System.out.println("GOAL IS REACHED!\n" + "Final list of facts is: "
+                    + dm.getListOfFacts());
+                System.out.println("Production System: " + dm.getCurrentProductionSystem());
+            } else {
+                System.out.println("The goal is not reached.");
+            }
         } catch (Exception e) {
             System.out.println("Something went wrong");
         }
--- src/forwardchaining/DataCollectorFromCommandLine.java
+++ src/forwardchaining/DataCollectorFromCommandLine.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package forwardchaining;
 
 import java.io.IOException;
@@ -10,19 +5,147 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- *
- * @author Dainius
+ * This class is for collecting data for application of production system.
+ * This class implement DataCollector inteface in that way that it collects data
+ * from command line.
+ * @author Dainius Jocas, VU MIF, PS#3, 3rd year
  */
 public class DataCollectorFromCommandLine implements DataCollector {
 
+    /**
+     * This method collects facts from the command line. Facts should be given
+     * in form "a, A, v, B". One fact is a letter between commas.
+     * @return ArrayList of facts
+     * @throws IOException
+     */
     public ArrayList<String> collectFacts() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        ArrayList<String> facts = new ArrayList();
+        boolean goodInput = true;
+        Scanner in = new Scanner(System.in);
+        String lineWithFacts = "";
+        System.out.println("Now You have to enter facts of the derivation system"
+                + ".\nFacts can be only letters and have to be in that format: "
+                + "A, b, c, D");
+        System.out.println("Enter facts:");
+        lineWithFacts = in.nextLine();
+        goodInput = isLineWithFactsCorrect(lineWithFacts);
+        while(!goodInput) {
+            System.out.println("Bad input! Try enter data once again, please.");
+            lineWithFacts = in.nextLine();
+            goodInput = isLineWithFactsCorrect(lineWithFacts);
+        }
+        facts = constructArrayListOfFacts(lineWithFacts);
+        return facts;
     }
 
+    /**
+     * This method from the String which has only letters, commas, and spaces
+     * builds Array list of facts.
+     * @param lineWithFacts
+     * @return ArrayList of Strings which represents facts.
+     */
+    private ArrayList<String> constructArrayListOfFacts(String lineWithFacts) {
+        ArrayList<String> facts = new ArrayList();
+        String[] arrayOfFacts = lineWithFacts.split(",");
+        for (String s: arrayOfFacts) {
+            s = s.trim();
+            if ((s.length() > 0) && (s.length() < 2) && (isGoalValid(s))) {
+                facts.add(s);
+            }
+        }
+        return facts;
+    }
+
+    /**
+     * This method checks if line consist only letters, commas, and spaces and
+     * checks if line has at least one letter.
+     * @param lineWithFacts
+     * @return true if facts are in right format, otherwise false
+     */
+    private boolean isLineWithFactsCorrect(String lineWithFacts) {
+        if ((lineWithFacts.matches("^[a-zA-Z ,]+$")) &&
+                (lineWithFacts.matches(".*[A-Za-z]+.*"))) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Method that collects implications from command line.
+     * @return ArrayList of implications, where one implication is a string of
+     * format R1: A -> B
+     * @throws IOException
+     */
     public ArrayList<String> collectImplications() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        ArrayList<String> implications = new ArrayList();
+        String implication = "";
+        String antecedent = "";
+        String consequent = "";
+        int i = 1;
+        System.out.println("Rule for the production system consists of two "
+                + "parts: antecedent and consequent.\n Antecedent is in format"
+                + " a, B, A. Consequent is one letter");
+        do {
+            System.out.print("Enter antecedent: ");
+            antecedent = getAntecedent();
+            System.out.print("Enter consequent: ");
+            consequent = getConsequent();
+            implication = "R" + i + ": " + antecedent + " -> " + consequent;
+            implications.add(implication);
+            System.out.println(implication);
+        } while (doWeHaveMoreImplications());
+
+        
+        return implications;
     }
 
+    /**
+     * Takes data from console and validates it.
+     * @return String which is antecedent for implication
+     */
+    private String getAntecedent() {
+        boolean goodInput = false;
+        Scanner in = new Scanner(System.in);
+        String lineWithFacts = in.nextLine();
+        goodInput = isLineWithFactsCorrect(lineWithFacts);
+        while(!goodInput) {
+            System.out.println("Bad input! Try enter data once again, please.");
+            lineWithFacts = in.nextLine();
+            goodInput = isLineWithFactsCorrect(lineWithFacts);
+        }
+        return lineWithFacts;
+    }
+
+    /**
+     * Takes data from console and validates it.
+     * @return string which is a consequent for implication.
+     */
+    private String getConsequent() {
+        Scanner in = new Scanner(System.in);
+        String consequent = in.nextLine();
+        boolean goodInput = isGoalValid(consequent);
+        /* Loop till we have a valid input */
+        while(!goodInput) {
+            System.out.println("Bad input! Try enter data once again, please.");
+            consequent = in.nextLine();
+            goodInput = isGoalValid(consequent);
+        }
+        return consequent;
+    }
+
+    /**
+     * Method to ask user if he wants to enter more implications
+     * @return
+     */
+    private boolean doWeHaveMoreImplications() {
+        System.out.println("If you want to enter more implications then press "
+                + "'y' else press 'n' and then 'ENTER'");
+        Scanner in = new Scanner(System.in);
+        if (('y' == (in.nextLine()).charAt(0)) || ('Y' == (in.nextLine()).charAt(0))) {
+            return true;
+        }
+        return false;
+    }
     /**
      * This method is for collecting a goal for the production system from
      * command line.
